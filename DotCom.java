353aa542ea5c6cfdd1f29b118f39e17c985c364e
Fakhriddin <faxa99deni@gmail.com>
2023-04-25 23:19:44 +0900

    my be mixed with codes from other 3 or 4chapters

---
+++ DotCom.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class DotCom {
+
+    public void setName(String string) {
+    }
+
+    public void setLocationCells(ArrayList<String> newLocation) {
+    }
+
+    public String checkYourself(String userGuess) {
+        return null;
+    }
+
+}
